a401ee18f6ff79d8f4eed7ee5b534719e61556b8
Jeong Gunhee <71108789+dyunasjk@users.noreply.github.com>
2021-03-19 11:31:46 +0900

    NewPackageTestFile

---
+++ src/lecture_210319/lecture_210319.java
@@ -0,0 +1,8 @@
+package lecture_210319;
+
+public class lecture_210319 {
+    public static void main(String[] args) {
+        System.out.println("Hello, JavaProgramming");
+        System.out.println("Just Test File");
+    }
+}
