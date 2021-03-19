801b596e774361851dd46f02bbab63d06224b2f2
Jeong Gunhee <71108789+dyunasjk@users.noreply.github.com>
2021-03-19 20:35:20 +0900

    Calculator_test_210319

--- src/lecture_210308/Calculator.java
+++ src/lecture_210308/Calculator.java
@@ -1,6 +1,7 @@
 package lecture_210308;
 
 import java.awt.*;
+import java.awt.Dimension.*;
 import java.awt.event.*;
 import javax.swing.*;
 import java.util.*;
@@ -22,14 +23,23 @@ public class Calculator extends JFrame implements ActionListener {
         //display.setEnabled(true);
 
         panel.setLayout(new GridLayout(0,5,3,3));
+        panel.setBackground(Color.gray);
+  
+        display.setBackground(Color.BLACK);
+        display.setForeground(Color.white);
+        display.setHorizontalAlignment(JTextField.RIGHT);
+        Font font = new Font("serif", Font.BOLD, 30);
+        display.setFont(font);
+  
         buttons = new JButton[25];
         int index = 0;
-        for (int rows = 0; rows < 5; rows++) {
+        for (int rows = 0; rows < 5; rows++) { 
             for (int cols = 0; cols < 5; cols++) {
                 buttons[index] = new JButton(labels[index]);
+                buttons[index].setPreferredSize(new Dimension(60, 50));
+                buttons[index].setBackground(Color.WHITE);
                 if (cols >= 3) buttons[index].setForeground(Color.red); //반복하면서 계산기의 버튼을 생성. 동일한 메소드로 이벤트 처리
                 else buttons[index].setForeground(Color.blue);
-                buttons[index].setForeground(Color.yellow);
                 panel.add(buttons[index]);
                 buttons[index].addActionListener(this);
                 index++;
@@ -38,9 +48,46 @@ public class Calculator extends JFrame implements ActionListener {
         add(display, BorderLayout.NORTH);
         add(panel, BorderLayout.CENTER);
         setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         pack();
     }
     public void actionPerformed(ActionEvent e) {
         String command = e.getActionCommand();
+        if (command.charAt(0) == 'C') { //클리어 버튼 처리
+            startOfNumber = true;
+            result = 0;
+            operator = "=";
+            display.setText("0.0");
+        } else if (command.charAt(0) >= '0' && command.charAt(0) <= '9' || command.equals(".")) {
+            if (startOfNumber == true) {
+                display.setText(command);
+            } else display.setText(display.getText() + command);
+            startOfNumber = false;
+        } else {
+            if (startOfNumber) {
+                if (command.equals("-")) {
+                    display.setText(command);
+                    startOfNumber = false;
+                } else operator = command; // 숫자입력이 시작되는 경우를 처리
+            } else {
+                double x = Double.parseDouble((display.getText()));
+                calculate(x);
+                operator = command;
+                startOfNumber = true; // 연산자 기호 입력시 연산 실시
+            }
+        }
+    }
+
+    private void calculate(double n) {
+        if (operator.equals("+")) result += n ;
+        else if (operator.equals("-")) result -= n;
+        else if (operator.equals("*")) result *= n;
+        else if (operator.equals("/")) result /= n;
+        else if (operator.equals("=")) result = n;
+        display.setText(" " + result);
+    }
+
+    public static void main(String[] args) {
+        Calculator s = new Calculator();
     }
 }
--- src/lecture_210319/test.java
+++ src/lecture_210319/test.java
@@ -10,14 +10,23 @@ public class test {
         String pw = "1111";
         try {
         login(id, pw);
-        } catch(Exception e){
+        } catch(LoginIDException e){
+            // 로그인 에러 처리 로직 실행
+            System.out.println(e.getMessage()+ ": " + e.getErrorCode());
+            if (e.getErrorCode() == 1) System.out.println("아이디, 암호 똑바로 입력해라");
+                
+        }
+        catch(LoginPasswordException e){
             // 로그인 에러 처리 로직 실행
             System.out.println(e.getMessage()+ ": " + e.getErrorCode());
             System.out.println("아이디, 암호 똑바로 입력해라");
         }
+        catch (Exception e) {
+            System.out.println(errCode());
+        }
     }
     
-    private static void login(String id, String pw) throws Exception{
+    private static void login(String id, String pw) throws LoginIDException, LoginPasswordException{
         // try {
         //     checkDB(id, pw);
         // } catch(Exception e) { // 오류처리
@@ -25,7 +34,7 @@ public class test {
         // }
         checkDB(id, pw);
     }
-    private static void checkDB(String id, String pw) throws Exception {
+    private static void checkDB(String id, String pw) throws LoginIDException, LoginPasswordException {
             if (id.equals("Jeong") == false ){
                 throw new Exception("ID Error"); 
             } else if (pw.equals("1111") == false) {
